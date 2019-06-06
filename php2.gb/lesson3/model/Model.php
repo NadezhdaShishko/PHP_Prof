f1a90f80652a9177e46bb57a15af3d7977c46f92
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-06-07 02:28:02 +0300

    model

--- php2.gb/lesson3/model/Model.php
+++ php2.gb/lesson3/model/Model.php
@@ -7,44 +7,67 @@ use app\engine\Db;
 
 abstract class Model implements IModel
 {
-    protected $db;
-
-    public function __construct()
-    {
-        $this->db = Db::getInstance();
-    }
-
     public function getWhere($name, $value) {
         $tableName = $this->getTableName();
-        $sql = "SELECT * FROM {$tableName} WHERE (name  = :name) = (value = :value)";
-        return $this->db->queryAll($sql, [':name' => $name, ':value' => $value]);
     }
 
-    public function getOne($id) {
-        $tableName = $this->getTableName();
+    public static function getOne($id) {
+        $tableName = static::getTableName();
         $sql = "SELECT * FROM {$tableName} WHERE id = :id";
-        return $this->db->queryOne($sql, ['id' => $id]);
+        return Db::getInstance()->queryObject($sql, ['id' => $id], get_called_class());
     }
     
-    public function getAll() {
-        $tableName = $this->getTableName();
+    public static function getAll() {
+        $tableName = static::getTableName();
         $sql = "SELECT * FROM {$tableName}";
-        return $this->db->queryAll($sql);
+        return Db::getInstance()->queryAll($sql);
     }
 
     public function insert() {
-        $tableName = $this->getTableName();
-        // foreach ($this->db as )
-        $sql = "INSERT INTO {$tableName} ()";
-        return $this->db->execute($sql, []);
-        $insert_id = $db->lastInsertId();
+        $tableName = static::getTableName();
+        $params = [];
+        $columns = [];
+        foreach ($this as $key => $value) {
+            if ($key == "id") continue;
+            $params[":{$key}"] = $value;
+            $columns[] = "`$key`";
+        }
+        $columns = implode(", ", $columns);
+        $values = implode(", ", array_keys($params));
+        $sql = "INSERT INTO {$tableName} ({$columns}) VALUES ({$values})";
+        Db::getInstance()->execute($sql, $params);
+        $this->id = Db::getInstance()->lastInsertId();
+        var_dump($sql);
     }
 
-    public function update() {}
-    
+    public function update() {
+        $tableName = static::getTableName();
+        $params = [];
+        $columns = [];
+        foreach ($this as $key => $value) {
+            if ($key == "id" || $key == "property") continue;
+            if ($this->property[$key] == false) continue;
+            $columns[] = "`$key` = :{$key}";
+            $params[":{$key}"] = $value;
+        }
+        $columns = implode(", ", $columns);
+        $params[':id'] = $this->id;
+        $sql = "UPDATE {$tableName} SET ({$columns}) WHERE id = :id";
+        Db::getInstance()->execute($sql, $params);
+        // $this->property();
+        var_dump($sql);
+    }
+
     public function delete() {
-        $tableName = $this->getTableName();
+        $tableName = static::getTableName();
         $sql = "DELETE FROM {$tableName} WHERE id = :id";
-        return $this->db->execute($sql, ['id' => $this->id = "insert_id"]);
+        return Db::getInstance()->execute($sql, ['id' => $this->id]);
+    }
+
+    public function save() {
+        if (is_null($this->id))
+            $this->insert();
+        else
+            $this->update();
     }
 }
