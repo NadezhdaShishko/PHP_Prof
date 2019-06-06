96714dfff861dabdad58447641c7764d9274365f
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-06-07 02:27:12 +0300

    user

--- php2.gb/lesson3/model/Users.php
+++ php2.gb/lesson3/model/Users.php
@@ -7,21 +7,31 @@ class Users extends Model
   public $id;
   public $login;
   public $pass;
+  public $property = [
+    'id' => false,
+    'login' => false,
+    'pass' => false
+];
 
   public function __construct($login = null, $pass = null)
     {
-        parent::__construct();
         $this->login = $login;
         $this->pass = $pass;
     }
 
-    public function insertUser() {
-        $tableName = $this->getTableName();
-        $sql = "INSERT INTO {$tableName} (login, pass) VALUES (:login, :pass)";
-        return $this->db->execute($sql, ['login' => $this->login, 'pass' => $this->pass]);
+    public function setLogin($name)
+    {
+        $this->property['login'] = true; 
+        $this->login = $login;
+    }
+ 
+    public function setPass($pass)
+    {
+        $this->property['pass'] = true;
+        $this->pass = $pass;
     }
 
-  public function getTableName() {
+  public static function getTableName() {
       return 'users';
   }
 }
