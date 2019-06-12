67ca61ce8903479deb858452006963a226e91b2c
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-06-12 13:37:36 +0300

    productmodel

---
+++ php2.gb/lesson5/model/Products.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\model;
+
+class Products extends DbModel
+{
+    public $id;
+    public $name;
+    public $description;
+    public $price;
+    public $property = [
+        'id' => false,
+        'name' => false,
+        'description' => false,
+        'price' => false
+    ];
+
+    public function __construct($name = null, $description = null, $price = null)
+    {
+        $this->name = $name;
+        $this->description = $description;
+        $this->price = $price;
+    }
+ 
+    public function setName($name)
+    {
+        $this->property['name'] = true; 
+        $this->name = $name;
+    }
+ 
+    public function setDescription($description)
+    {
+        $this->property['description'] = true;
+        $this->description = $description;
+    }
+ 
+    public function setPrice($price)
+    {
+        $this->property['price'] = true;
+        $this->price = $price;
+    }
+
+     public function propertyFalse() {
+         $this->property['id'] = false;
+         $this->property['name'] = false;
+         $this->property['description'] = false;
+         $this->property['price'] = false;
+     }
+
+    public static function getTableName() {
+        return 'products';
+    }
+}
