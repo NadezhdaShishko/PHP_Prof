020cf53a669529bb844f7bf767038fe8004f7297
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-06-12 13:43:14 +0300

    indexpublic

---
+++ php2.gb/lesson5/public/index.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+require "../engine/Autoload.php";
+require "../config/config.php";
+
+use app\model\Products;
+use app\engine\Autoload;
+use app\engine\Render;
+use app\engine\TwigRender;
+
+require_once '../vendor/autoload.php';
+spl_autoload_register([new Autoload(), 'loadClass']);
+
+
+$controllerName = $_GET['c'] ?: 'product';
+$actionName = $_GET['a'];
+
+$controllerClass = CONTROLLER_NAMESPACE . ucfirst($controllerName) . "Controller";
+if (class_exists($controllerClass)) {
+    $controller = new $controllerClass(new Render());
+    $controller->runAction($actionName);
+} else {
+    echo "404";
+}
+
+/** @var Products $product */
+
+// $product = Products::getOne(19);
+// $product->setName("Mountain Dew");
+//
+// $product->save();
+// var_dump($product);
+
+// $product = new Products("7Up", "Напиток газированный", 60);
+// $product->save();
+
+// $product = Products::getOne(14);
+// $user = Users::getOne(5);
+// $user->delete();
