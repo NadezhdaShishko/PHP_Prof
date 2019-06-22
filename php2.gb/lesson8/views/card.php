4f7dad0d81963ed14e5fe6741e4d0b4634da4b27
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-06-22 18:54:22 +0300

    Заглушка. В процессе.

---
+++ php2.gb/lesson8/views/card.php
@@ -0,0 +1,9 @@
+<?php
+/** @var \app\models\Products $product  */
+?>
+
+<h2><?= $product->name ?></h2>
+<p><?= $product->description ?></p>
+<p>Стоимость: <?= $product->price ?> руб.</p>
+
+<button data-id="<?=$product->id?>" class="action">Купить</button><br>
