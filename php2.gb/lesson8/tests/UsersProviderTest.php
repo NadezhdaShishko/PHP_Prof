e3fdd7bbdcdc56fab2b01719ca60933c5f9f0347
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-06-22 18:51:57 +0300

    Заглушка. В процессе.

---
+++ php2.gb/lesson8/tests/UsersProviderTest.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace app\tests;
+
+use app\models\entities\Users;
+use PHPUnit\Framework\TestCase;
+
+class UsersProviderTest extends TestCase
+{
+//    /**
+//     * @param $login
+//     * @param $pass
+//     * @param $hash
+//     * @dataProvider providerUser
+//     */
+//
+//    public function TestUsers($login, $pass, $hash) {
+//        $user = new Users($login, $pass, $hash);
+//        $this->assertEquals('login', $user->getLogin());
+//        $this->assertEquals('pass', $user->getPass());
+//        $this->assertEquals('hash', $user->getHash());
+//    }
+//
+//    public function providerUsers(){
+//        return [
+//            ['admin', 'admin', ''],
+//            ['Люси', 'gavGav123', '']
+//        ];
+//    }
+}
