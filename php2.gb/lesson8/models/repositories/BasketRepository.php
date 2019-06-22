37306cbe4af3b7af3e6cb3a89aa1847dd9d20b40
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-06-22 18:51:03 +0300

    Заглушка. В процессе.

---
+++ php2.gb/lesson8/models/repositories/BasketRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\models\repositories;
+
+use app\models\Repository;
+use app\models\entities\Basket;
+
+class BasketRepository extends Repository
+{
+    public function getBasket($session) {
+        $sql = "SELECT p.id id_prod, b.id id_basket, p.name, p.description, p.price FROM basket b, products p WHERE b.product_id=p.id AND session_id = :session";
+        return $this->db->queryAll($sql, ['session' => $session]);
+    }
+
+    public function getCount($session) {
+        $sql = "SELECT count(*) as count FROM `basket` WHERE `session` = :session";
+        return $this->db->queryOne($sql, ['session' => $session])['count'];
+    }
+
+    public function getEntityClass() {
+        return Basket::class;
+    }
+
+    public function getTableName() {
+        return 'basket';
+    }
+
+}
