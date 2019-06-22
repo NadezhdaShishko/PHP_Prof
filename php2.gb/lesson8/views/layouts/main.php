7ae5ea84ff7b75844b6115309fd66edc1ab981e8
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-06-22 18:54:37 +0300

    Заглушка. В процессе.

---
+++ php2.gb/lesson8/views/layouts/main.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+</head>
+<body>
+<a href="/">Главная</a>
+<a href="/product/catalog">Каталог</a>
+<a href="/basket">Корзина</a> <span id="count"><?=$count?></span><br><br>
+<?=$topmenu?>
+<?=$content?>
+<script src="/js/main.js"></script>
+</body>
+</html>
