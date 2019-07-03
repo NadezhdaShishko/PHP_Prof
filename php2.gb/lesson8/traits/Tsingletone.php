143bc870685929d5728d940d1c54faa2acbaa708
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-07-03 21:01:30 +0300

    Tsingletone

--- php2.gb/lesson8/traits/Tsingletone.php
+++ php2.gb/lesson8/traits/Tsingletone.php
@@ -5,6 +5,7 @@ namespace app\traits;
 trait Tsingletone
 {
     private static $instance = null;
+
     public static function getInstance() {
         if (is_null (static::$instance)) {
             static::$instance = new static();
