18e3d516bcd8d03f65b8f84294452777f436125d
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-07-03 21:02:42 +0300

    views

--- php2.gb/lesson8/views/basket.php
+++ php2.gb/lesson8/views/basket.php
@@ -3,10 +3,12 @@
 <?php
 foreach ($products as $product):?>
     <div id="<?= $product['id_basket'] ?>">
-        <h2><?= $product['name'] ?></h2></a>
+        <h2><a href="/product/card/?id=<?= $product['id_prod'] ?>"><?= $product['name'] ?></a></h2>
         <p>Описание: <?= $product['description'] ?></p>
         <p>Цена: <?= $product['price'] ?> руб.</p>
 
         <button data-id="<?= $product['id_basket']?>" class="delete">Удалить</button>
     </div>
 <? endforeach;?>
+<br>
+<a href="/product/catalog">Каталог</a>
