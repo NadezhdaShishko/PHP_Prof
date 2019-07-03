328a9853755b31f271756eea4d52852b9e98a94b
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-07-03 21:00:46 +0300

    index.php

--- php2.gb/lesson8/public/index.php
+++ php2.gb/lesson8/public/index.php
@@ -11,35 +11,17 @@ if (!isset($_SESSION['times'])) {
     $_SESSION['times']++;
 }
 
-require "../engine/Autoload.php";
-require "../config/config.php";
+//require "../engine/Autoload.php";
+//require "../config/config.php";
+require_once '../vendor/autoload.php';
+$config = include __DIR__ . "/../config/config.php";
 
-//use app\engine\Autoload;
-use app\engine\Render;
-use app\engine\Request;
-//use app\engine\TwigRender;
+use app\engine\Autoload;
+use app\engine\App;
 
-try {
-    require_once '../vendor/autoload.php';
 //    spl_autoload_register([new Autoload(), 'loadClass']);
-
-    $request = new Request();
-
-    $controllerName = $request->getControllerName() ?: 'product';
-    $actionName = $request->getActionName();
-
-    $controllerClass = CONTROLLER_NAMESPACE . ucfirst($controllerName) . "Controller";
-
-    if (class_exists($controllerClass)) {
-        $controller = new $controllerClass(new Render());
-        $controller->runAction($actionName);
-    } else {
-        throw new \Exception("Контроллер не существует", 404);
-    }
-} catch (\PDOException $e) {
+try {
+    App::call()->run($config);
+} catch (Exception $e) {
     var_dump($e);
-
-} catch (\Exception $e) {
-    echo $e->getMessage();
-    var_dump($e->getTrace());
 }
