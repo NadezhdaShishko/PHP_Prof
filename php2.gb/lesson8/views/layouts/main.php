24e550efdeb95ab55f44a9e07f49e51b4b0da6c7
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-07-03 21:02:54 +0300

    views

--- php2.gb/lesson8/views/layouts/main.php
+++ php2.gb/lesson8/views/layouts/main.php
@@ -8,8 +8,19 @@
 <body>
 <a href="/">Главная</a>
 <a href="/product/catalog">Каталог</a>
-<a href="/basket">Корзина</a> <span id="count"><?=$count?></span><br><br>
-<?=$topmenu?>
+<a href="/basket">Корзина</a> <span id="count"><?=$count?></span>
+
+<? if ($auth): ?>
+    Добро пожаловать <?=$username?> <a href="/user/logout/"> [Выход]</a>
+<? else: ?>
+    <form action="/user/login/" method="post">
+        <input type="text" name="login" placeholder="Логин">
+        <input type="password" name="pass" placeholder="Пароль">
+        <input type="submit" name="submit" value="Войти">
+    </form>
+<? endif; ?>
+<br>
+
 <?=$content?>
 <script src="/js/main.js"></script>
 </body>
